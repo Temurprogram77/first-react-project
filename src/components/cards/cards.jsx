5de4c5babd1202a1120471c1e320a644ba9918a8
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-04 12:47:16 +0300

    added

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -4,7 +4,17 @@ import "./cards.css";
 function Cards() {
   return (
     <div className="container">
-      <div className="card"></div>
+      <div className="card">
+        <div className="left_part">
+          <div className="btns">
+            <div className="first_btn">Для путешествий</div>
+          </div>
+          <h2>Visa Classic</h2>
+          <div className="writing"></div>
+          <div className="button">Подробнее</div>
+        </div>
+        <div className="right_part"></div>
+      </div>
     </div>
   );
 }
