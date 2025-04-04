74633fcbc44c56c7751f0bfead6f8df398e9606a
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-04 20:38:33 +0300

    added

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,14 @@ import React from "react";
 const Card = (props) => {
   return (
     <div>
-        
+        <div className="cards">
+            <FaRegFileAlt size={24} color="black" />
+            <h4 className="title">Отчет за II квартал 2024 года</h4>
+            <div className="flex-div">
+                <p>PDF, 440 Kb</p>
+                <div className="">x</div>
+            </div>
+        </div>
     </div>
   );
 };
