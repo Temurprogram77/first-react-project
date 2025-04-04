5710282e090f4f74f7cac4eba40a9e297ee22551
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-04 20:18:47 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Cards from './components/cards/cards.jsx'
-import Navbar from './components/navbar/navbar.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import icon from "./assets/images/logo.svg";
+import "./style.css"
+
+const base = [{}];
 
 const Home = () => {
+  return <Navbar />;
+};
+
+const Navbar = () => {
   return (
-    <></>
-  )
-}
+    <div>
+      <div className="container box">
+        <div className="left_part">
+          <a href="#">
+            <img src={icon} alt="HamkorBank" />
+          </a>
+          <div className="pages">
+            <a href="#">Кредиты</a>
+            <a href="#">Карты</a>
+            <a href="#">Микрозаймы</a>
+            <a href="#">Вклады</a>
+            <a href="#">Курс валют</a>
+            <a href="#">Еще</a>
+          </div>
+        </div>
+        <div className="right_part">
+          <div className="money">
+            ${" "}
+            <div className="first">
+              <span>Купить</span> 12 860
+            </div>
+            <div className="second">
+              {" "}
+              <span>Продать</span> 12 970
+            </div>
+          </div>
+          <div className="btn">Интернет-банк</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 
-const rootElement = document.getElementById('root')
-const root = ReactDOM.createRoot(rootElement)
-
-root.render(<Home />)
+root.render(<Home />);
