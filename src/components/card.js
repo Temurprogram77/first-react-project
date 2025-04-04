ab6b6a2fb7a7163c59febf1f1f0d0773195ab473
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-04 21:06:25 +0300

    added

--- src/components/card.js
+++ src/components/card.js
@@ -4,16 +4,12 @@ import { FaRegFileAlt } from "react-icons/fa";
 
 const Card = (props) => {
   return (
-    <div>
-      <div className="container cards">
-        <div className="card">
-          <FaRegFileAlt size={24} color="black" />
-          <h4 className="title">{props.cardTitle}</h4>
-          <div className="flex-div">
-            <p>PDF, 440 Kb</p>
-            <div className="icon">x</div>
-          </div>
-        </div>
+    <div className="card">
+      <FaRegFileAlt size={24} color="black" />
+      <h4 className="title">{props.cardTitle}</h4>
+      <div className="flex-div">
+        <p>PDF, 440 Kb</p>
+        <div className="icon">x</div>
       </div>
     </div>
   );
