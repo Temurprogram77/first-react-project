4ab7b2a112bb9633a1001d6473c06b2568a3293a
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-04 13:22:31 +0300

    added

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -13,7 +13,24 @@ function Cards() {
             <div className="third_btn">EUR</div>
           </div>
           <h2>Visa Classic</h2>
-          <div className="writing"></div>
+          <div className="writing">
+            <div className="one">
+              <h3>бесплатно</h3>
+              <p>Оформление карты</p>
+            </div>
+            <div className="two">
+              <h3>без комиссии</h3>
+              <p>
+                Снятие наличных, переводы и оплата через терминалы Hamkorbank
+              </p>
+            </div>
+            <div className="three">
+              <h3>быстрые переводы</h3>
+              <p>
+              Отправим в течение минуты
+              </p>
+            </div>
+          </div>
           <div className="button">Подробнее</div>
         </div>
         <div className="right_party">
