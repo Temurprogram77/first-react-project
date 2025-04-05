6c2b71e1ccbec2a1435fd74c51a0da7035d04c1f
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-05 16:42:45 +0300

    added

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,13 +3,14 @@ import "./card.css";
 import { FaRegFileAlt, FaCheck, FaTimes } from "react-icons/fa";
 
 const Card = (props) => {
+  const {cardTitle,cardVolume,download}=props;
   return (
     <div className="card">
       <FaRegFileAlt size={24} color="black" />
-      <h4 className="title">{props.cardTitle}</h4>
+      <h4 className="title">{cardTitle}</h4>
       <div className="flex-div">
-        <p>{props.cardVolume}</p>
-        <div className="icon">{props.download===true?<FaCheck color="green" />: <FaTimes color="red" />}</div>
+        <p>{cardVolume}</p>
+        <div className="icon">{download===true?<FaCheck color="green" />: <FaTimes color="red" />}</div>
       </div>
     </div>
   );
