5f2eda593847a7d60c058c8677696ed8eb29a099
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-07 14:39:09 +0300

    added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Cards from "./components/card/card.js";
 import Navbar from "./components/navbar/navbar.js";
-import Footer from "./components/footer/footer.js";
+import Footer from "./components/footer/footer.jsx";
 
 const base = [
   {
